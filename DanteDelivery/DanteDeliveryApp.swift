7760a5894b12d5fa4eee88b116ed94257bd96830
DanielDante15 <danieldantefm@gmail.com>
2023-12-30 21:31:21 -0300

    Initial Commit

---
+++ DanteDelivery/DanteDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DanteDeliveryApp.swift
+//  DanteDelivery
+//
+//  Created by Daniel Macedo on 30/12/23.
+//
+
+import SwiftUI
+
+@main
+struct DanteDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
